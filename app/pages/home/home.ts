fdc47c0c46ce25209accddb74b281a58166e3c5e
codeitloadit <brian@codeitload.it>
2016-05-19 00:56:20 -0700

    Saving changes

--- app/pages/home/home.ts
+++ app/pages/home/home.ts
@@ -1,11 +1,21 @@
 import {Page} from 'ionic-angular';
+import {Http} from 'angular2/http';
+import 'rxjs/add/operator/map';
 
 
 @Page({
-  templateUrl: 'build/pages/home/home.html'
+    templateUrl: 'build/pages/home/home.html'
 })
 export class HomePage {
-  constructor() {
-
-  }
+    http;
+    platform;
+    rooms;
+    genderTab;
+    constructor(http: Http) {
+        this.genderTab = 'a';
+        this.http = http;
+        this.http.get('http://localhost:8000/cbaff.json').map(res => res.json()).subscribe(data => {
+            this.rooms = data.slice(0, 90);
+        });
+    }
 }
